cbb947fd1838cdae8ae3cf95f3e7f64721c548f2
Xat_MassacrE <ywt13639397661@gmail.com>
2018-09-28 16:54:36 +0800

    Add get signature method

--- controllers/bet.js
+++ controllers/bet.js
@@ -1,13 +1,49 @@
-var express = require('express')
-var router = express.Router()
-var conf = require('../config/config.js')
-var redisPool = require("../redis_pool");
+const express = require('express')
+const router = express.Router()
+const Account = require("eth-lib/lib/account")
+const conf = require('../config/config.js')
+const redisPool = require("../redis_pool");
+const web3 = conf.getWeb3();
 
-router.get("/saveSecret", function (req, res, next) {
+// router.get("/saveSecret", function (req, res, next) {
+//     let success = conf.requestSuccess()
+//     let fail = conf.requestFaild()
+//     const commit = req.query.commit
+//     const reveal = req.query.reveal
+//     console.log(commit, reveal)
+//     redisPool.set(commit, reveal)
+//     res.json(success)
+// })
+
+router.get("/random", function (req, res, next) {
     let success = conf.requestSuccess()
     let fail = conf.requestFaild()
-    console.log(req.query)
+    const networkId = req.query.networkId
+    const address = req.query.address
+		const reveal = String(Math.random()).slice(2,18)
+    const commit = web3.utils.soliditySha3(reveal)
+    redisPool.set(commit, reveal)
+    const expiredBlockNumber = 8000000
+    signData = getSignature(expiredBlockNumber, commit, conf.pk)
+    data = {}
+		data.commit = commit
+    data.expiredBlockNumber = expiredBlockNumber
+    data.signature = {}
+    data.signature.signature = signData[0]
+    data.signature.v = signData[1]
+    data.signature.r = signData[2]
+    data.signature.s = signData[3]
+    success.data = data
     res.json(success)
 })
+
+function getSignature(expiredBlockNumber, commit, pk) {
+  const msgHash = web3.utils.soliditySha3(expiredBlockNumber, commit)
+  const signature = Account.sign(msgHash, pk)
+  const r = signature.slice(0, 66)
+  const s = '0x' + signature.slice(66, 130)
+  const v = signature.slice(130, 132)
+  return [signature, v, r, s]
+}
 
 module.exports = router
