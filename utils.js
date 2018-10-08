f21f2dd357dcc30502bd1234bfcab41814fa3fd3
Xat_MassacrE <ywt13639397661@gmail.com>
2018-10-08 14:53:54 +0800

    Add utils to get final result from reveal and blockHash

---
+++ utils.js
@@ -0,0 +1,18 @@
+const web3 = require('web3')
+const BigNumber = require('bignumber.js')
+
+function getResult(reveal, blockHash, module) {
+  const random = new BigNumber(web3.utils.soliditySha3(reveal, blockHash))
+  const result = random.modulo(module)
+  return result.toNumber()
+}
+
+const reveal = '0x' + '000000000000000000000000000000000000000000000000000ddee4def596f7'
+const commit = web3.utils.sha3(reveal)
+const blockHash = '0xac62d92f3c1eec9b2574db3a06c6b3cba28628bc4bb47b640e72515eaa9d65d3'
+console.log(commit)
+console.log(getResult(reveal, blockHash, 99))
+
+module.exports = {
+  getResult: getResult
+}
