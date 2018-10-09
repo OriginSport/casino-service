c88a88edf91eb68849aa5b9cc2d92d349e7a157c
Xat_MassacrE <ywt13639397661@gmail.com>
2018-10-09 20:05:55 +0800

    Add trust proxy

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('trust proxy', true);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -26,6 +27,9 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  //res.header("Access-Control-Allow-Origin")
+  //res.header("Access-Control-Allow-Credentials", "true")
+  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next(createError(404));
 });
 
