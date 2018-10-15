d6fb794f4bfa4e8c214c9a6801d120ad86d8e18a
Xat_MassacrE <ywt13639397661@gmail.com>
2018-10-16 01:57:36 +0800

    Rename config to conf

--- manageService.js
+++ manageService.js
@@ -1,21 +1,21 @@
-var redisPool = require("./redis_pool")
-const config = require('./config/config')
+var redisPool = require("./redisPool")
+const conf = require('./config/config')
 
 var Tx = require('ethereumjs-tx')
 const {promisify} = require('util');
 const getAsync = promisify(redisPool.get).bind(redisPool);
 
-const web3 = config.getWeb3();
+const web3 = conf.getWeb3();
 
 async function syncNonce(from) {
-  const key = 'NONCE:' + config.debug + ':' + from
+  const key = 'NONCE:' + conf.debug + ':' + from
   const nonce = await web3.eth.getTransactionCount(from)
   console.log(nonce)
   redisPool.set(key, nonce, 'EX', 3600*24)
 }
 
 async function getNonce(from) {
-  const key = 'NONCE:' + config.debug + ':' + from
+  const key = 'NONCE:' + conf.debug + ':' + from
   const res = await getAsync(key)
   if (!res) {
     const nonce = await web3.eth.getTransactionCount(from)
@@ -30,8 +30,8 @@ async function getNonce(from) {
 
 async function closeBet(commit, callback) {
   const reveal = await getAsync(commit)
-  const pk = config.pk
-  const from = config.address
+  const pk = conf.pk
+  const from = conf.address
   const gasPrice = await web3.eth.getGasPrice()
   let nonce = await getNonce(from)
   const nonceOnline = await web3.eth.getTransactionCount(from)
@@ -41,14 +41,14 @@ async function closeBet(commit, callback) {
     nonce = nonceOnline
   }
   var privateKey = new Buffer(pk, 'hex')
-  var data = config.casinoContract.methods.closeBet(reveal).encodeABI()
+  var data = conf.casinoContract.methods.closeBet(reveal).encodeABI()
 
   var rawTx = {
     nonce: web3.utils.toHex(nonce),
     gasPrice: web3.utils.toHex(gasPrice), // 2 Gwei 2000000000
     gasLimit: web3.utils.toHex(60000),
     from: from,
-    to: config.casinoContract._address,
+    to: conf.casinoContract._address,
     value: web3.utils.toHex(0),
     data: data
   }
@@ -60,7 +60,7 @@ async function closeBet(commit, callback) {
   return web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'), callback)
 }
 
-syncNonce(config.address)
+syncNonce(conf.address)
 //closeBet('0x36c3b7aa855b06e4c0d38c31b88edb823ca0af6dcc555cc208404e11877653c8', console.log)
 //closeBet('0x' + 'bfbca8ab55b013bea34cb7ed0436b4069d90b00d271eee0d6b627514e0c117df', console.log)
 module.exports = {
