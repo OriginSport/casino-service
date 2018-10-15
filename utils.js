68d230b32c242fe1ad0a004d50e3d709c2b5079a
Xat_MassacrE <ywt13639397661@gmail.com>
2018-10-16 02:08:36 +0800

    Remove unused log

--- utils.js
+++ utils.js
@@ -10,8 +10,8 @@ function getResult(reveal, blockHash, modulo) {
 const reveal = '0x' + '000000000000000000000000000000000000000000000000000ddee4def596f7'
 const commit = web3.utils.sha3(reveal)
 const blockHash = '0xac62d92f3c1eec9b2574db3a06c6b3cba28628bc4bb47b640e72515eaa9d65d3'
-console.log(commit)
-console.log(getResult(reveal, blockHash, 99))
+//console.log(commit)
+//console.log(getResult(reveal, blockHash, 99))
 
 module.exports = {
   getResult: getResult
