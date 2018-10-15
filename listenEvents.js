29d92fc11a1a1bdcf2db129c169f1d971bf32d10
Xat_MassacrE <ywt13639397661@gmail.com>
2018-10-16 01:58:13 +0800

    Add redlock to lock closeBet nonce

--- listenEvents.js
+++ listenEvents.js
@@ -1,22 +1,20 @@
 const axios = require('axios')
 const Web3 = require('web3')
 
-const redisPool = require('./redis_pool')
+const redisPool = require('./redisPool')
 const redisUtils = require('./redisUtils')
 const conf = require('./config/config.js')
 const manage = require('./manageService')
 const utils = require('./utils')
+const redlock = require('./redlock')
 
 let debug = true
 if (process.env.NODE_ENV == 'pro') {
   debug = false
 }
 
-//web3.setProvider(new Web3.providers.WebsocketProvider('ws://' + conf.geth_http_host + ':' + conf.geth_ws_port, 5000));
-const ws_url = debug ? 'wss://ropsten.infura.io/_ws' : 'wss://mainnet.infura.io/_ws'
-
 const getProvider = () => {
-    const provider = new Web3.providers.WebsocketProvider(ws_url)
+    const provider = new Web3.providers.WebsocketProvider(conf.geth_ws_url)
     provider.on('connect', () => console.log(new Date().toLocaleString(), ' WebSocket -------start--------'))
     provider.on('error', e => {
       console.error(new Date().toLocaleString(), ' WS Error: ', e)
@@ -85,44 +83,55 @@ function listenPlaceBet() {
       pushUpdate(dataUpdateUrl, 'dataUpdate')
 
 
-      redisPool.get(commit, function(err, reply) {
-        if (!reply) {
-          return
-        }
-        const result = utils.getResult(reply, blockHash, modulo)
-        manage.closeBet(commit, function(err, hash) {
-          if (err) {
-            console.log('[' + new Date().toLocaleString() + ']:' + 'close bet error occur, has return: ', err)
+      const ttl = 1000
+      redlock.lock(conf.redlockKey, ttl).then(lock => {
+        redisPool.get(commit, function(err, reply) {
+          if (!reply) {
             return
           }
-          redisPool.incr('NONCE:' + conf.debug + ':' + conf.address, function(err, reply) {
-            console.log('incr nonce ', err, reply, hash)
-            redisPool.del(commit)
+          const result = utils.getResult(reply, blockHash, modulo)
+          manage.closeBet(commit, function(err, hash) {
+            if (err) {
+              console.log('[' + new Date().toLocaleString() + ']:' + 'close bet error occur, has return: ', err)
+              return
+            }
+            redisPool.incr('NONCE:' + conf.debug + ':' + conf.address, function(err, reply) {
+              console.log('incr nonce ', err, reply, hash)
+              redisPool.del(commit)
 
-            const commitRevealUrl = 'http://' + conf.inetAddr + ':' + conf.inetPort +  '/v1/api/betClosed?commit=' + commit + '&result=' + result + '&modulo=' + modulo
-            pushUpdate(commitRevealUrl, 'commit-reveal')
+              const commitRevealUrl = 'http://' + conf.inetAddr + ':' + conf.inetPort +  '/v1/api/betClosed?commit=' + commit + '&result=' + result + '&modulo=' + modulo
+              pushUpdate(commitRevealUrl, 'commit-reveal', modulo, result)
+            })
+          }).catch(error => {
+            console.log('[' + new Date().toLocaleString() + ']:' + 'close bet error occur: ', error)
           })
-          //io.sockets.emit('closeBet', commit)
-        }).catch(error => {
-          console.log('[' + new Date().toLocaleString() + ']:' + 'close bet error occur: ', error)
+        })
+        return lock.unlock(error => {
+          if (error) {
+            console.log('[' + new Date().toLocaleString() + ']:', error)
+          }
         })
       })
     }
   })
 }
 
-function pushUpdate(url, logText) {
+function pushUpdate(url, logText, arg1, arg2) {
   axios.get(url)
     .then(function (response) {
-      console.log('push ' + logText + ' to client');
+      console.log('push ' + logText + ' to client', arg1, arg2);
     })
     .catch(function (error) {
       console.log(logText + '[' + new Date().toLocaleString() + ']:', error);
     })
 }
 
-listenPlaceBet()
-listenbetClosed()
+function startListen() {
+  listenPlaceBet()
+  listenbetClosed()
+}
+
+startListen()
 
 module.exports = {
   listenPlaceBet: listenPlaceBet
