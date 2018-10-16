91be3c7eaa6d1d97262f8d3020744deece6e92e3
Xat_MassacrE <ywt13639397661@gmail.com>
2018-10-16 17:20:21 +0800

    Add pushUpdate function to utils

--- utils.js
+++ utils.js
@@ -1,10 +1,21 @@
 const web3 = require('web3')
+const axios = require('axios')
 const BigNumber = require('bignumber.js')
 
 function getResult(reveal, blockHash, modulo) {
   const random = new BigNumber(web3.utils.soliditySha3(reveal, blockHash))
   const result = random.modulo(modulo)
   return result.toNumber()
+}
+
+function pushUpdate(url, logText) {
+  axios.get(url)
+    .then(function (response) {
+      console.log('push ' + logText + ' to client');
+    })
+    .catch(function (error) {
+      console.log(logText + '[' + new Date().toLocaleString() + ']:', error);
+    })
 }
 
 const reveal = '0x' + '000000000000000000000000000000000000000000000000000ddee4def596f7'
@@ -14,5 +25,6 @@ const blockHash = '0xac62d92f3c1eec9b2574db3a06c6b3cba28628bc4bb47b640e72515eaa9
 //console.log(getResult(reveal, blockHash, 99))
 
 module.exports = {
-  getResult: getResult
+  getResult: getResult,
+  pushUpdate: pushUpdate
 }
